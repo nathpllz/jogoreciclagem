7c61f4fa1b2647948b7ffab4805579ef702954ea
nathpllz <161333624+nathpllz@users.noreply.github.com>
2025-11-04 14:36:43 -0300

    d

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const items = [
   { name: "Garrafa plástica", type: "trash" },
   { name: "Restos de comida", type: "org" },
   { name: "Lata de refrigerante", type: "trash" },
-  { name: "carne", type: "trash" }
+  { name: "carne", type: "trash" },
+  { name: "Casca de ovo", type: "org" },
   { name: "Folhas secas", type: "org" },
   { name: "Isopor", type: "trash" },
 ];
